5f14bd5415d5b362a6a26d1f70264fd6a2ac3279
Camila Rojas <90579964+camilarojas11@users.noreply.github.com>
2022-08-22 18:23:11 -0300

    segundo commit

--- homebanking/src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ homebanking/src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -5,6 +5,7 @@ import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
+import com.mindhub.homebanking.utils.AccountUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +29,7 @@ public class ClientController {
     AccountRepository accountRepository;
 
     @GetMapping("/api/clients")
-    public List<ClientDTO> getAll(){
+    public List<ClientDTO> getAll() {
 
         return clientRepository.findAll().stream().map(client -> new ClientDTO(client)).collect(Collectors.toList());
     }
@@ -39,39 +40,37 @@ public class ClientController {
     }*/
 
     @GetMapping("api/clients/{id}")
-    public ClientDTO getClientById(@PathVariable Long id){
+    public ClientDTO getClientById(@PathVariable Long id) {
         return new ClientDTO(clientRepository.findById(id).get());
     }
 
     @GetMapping("/api/clients/current")
-    public ClientDTO getClient(Authentication authentication){
+    public ClientDTO getClient(Authentication authentication) {
         Client client = this.clientRepository.findByEmail(authentication.getName());
         return new ClientDTO(client);
     }
 
-        @Autowired
-        private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-        @PostMapping("/api/clients")
-        public ResponseEntity<Object> register(
-                @RequestParam String firstName, @RequestParam String lastName,
-                @RequestParam String email, @RequestParam String password) {
+    @PostMapping("/api/clients")
+    public ResponseEntity<Object> register(
+            @RequestParam String firstName, @RequestParam String lastName,
+            @RequestParam String email, @RequestParam String password) {
 
-            if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
-                return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
-            }
-
-            if (clientRepository.findByEmail(email) !=  null) {
-                return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
-            }
-
-            clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password)));
-            //Creo la cuenta para el cliente nuevo, para asignarlo al cliente en cuestion, lo hago por findByEmail(email) --> hace referencia al cliente creado.
-            accountRepository.save(new Account("VIN-" + getRandomAccountNumber(10000000, 100000001), LocalDateTime.now(), 0.00, clientRepository.findByEmail(email)));
-            return new ResponseEntity<>("Account created", HttpStatus.CREATED);
-
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
         }
 
-    public int getRandomAccountNumber(int min, int max){ return (int) ((Math.random() * (max - min)) + min);}
+        if (clientRepository.findByEmail(email) != null) {
+            return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
+        }
 
+        clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password)));
+        //Creo la cuenta para el cliente nuevo, para asignarlo al cliente en cuestion, lo hago por findByEmail(email) --> hace referencia al cliente creado.
+        accountRepository.save(new Account("VIN-" + AccountUtils.createAccountNumber(10000000, 100000001), LocalDateTime.now(), 0.00, clientRepository.findByEmail(email)));
+        return new ResponseEntity<>("Account created", HttpStatus.CREATED);
+
+    }
 }
+
